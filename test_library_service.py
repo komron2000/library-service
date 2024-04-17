6499826902df7f0730e3dcf1e8ad7a2957ed7894
Kamron200 <96528746+komron2000@users.noreply.github.com>
2024-04-17 17:41:57 +0300

    3

--- test_library_service.py
+++ test_library_service.py
@@ -1,24 +1,9 @@
 import requests
 
 
-class LibrariesAPI:
-    def __init__(self, base_url: str):
-        self.base_url = base_url
-
-    def get_all_clothes(self):
-        url = self.base_url
-        res = requests.get(url).json()
-        return res
-
-    def get_cloth_by_id(self, cloth_id: int):
-        url = f"{self.base_url}{cloth_id}"
-        res = requests.get(url).json()
-        return res
-
-
-def test_get_all_clothes(api: LibrariesAPI):
-    res = api.get_all_clothes()
-    assert (res == [{'library_id': 1,
+def testgetall_libraries(url: str):
+    res = requests.get(url).json()
+    assert(res == [{'library_id': 1,
               'name': 'Central Library',
               'address': '123 Main Street',
               'square_meters': 5000,
@@ -44,26 +29,17 @@ def test_get_all_clothes(api: LibrariesAPI):
               'square_meters': 3800,
               'city': 'Tokyo'}])
 
-def test_get_cloth_by_id(api: LibrariesAPI):
-    res = api.get_cloth_by_id(1)
-    assert (res == {'library_id': 1,
+
+def test_get_library_by_id(url: str):
+    res = requests.get(url).json()
+    assert(res == {'library_id': 1,
               'name': 'Central Library',
               'address': '123 Main Street',
               'square_meters': 5000,
               'city': 'New York'})
 
 
-def test_get_cloth_by_name(api: LibrariesAPI):
-    # Проверка получения информации по имени
-    clothes = api.clothes()
-    clothes_name = clothes[0]['name']  # Предполагаем, что компания существует в списке
-    res = api.get_cloth_by_name(clothes_name)
-    assert (res['name'] == clothes_name)
-
-
-if __name__ == '__main__':
-    URL = 'http://127.0.0.1:80/api/v1/libraries/'
-    api = LibrariesAPI(URL)
-    test_get_cloth_by_id(api)
-    test_get_all_clothes(api)
-    test_get_cloth_by_name(api)
+if __name == '__main':
+    URL = 'http://127.0.0.1/api/v1/libraries/'
+    test_get_library_by_id(URL + '1')
+    test_get_all_libraries(URL)
